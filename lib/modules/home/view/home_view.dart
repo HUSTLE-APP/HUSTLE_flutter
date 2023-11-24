bba8e5fa83e7ab387ab857b740408c976b176240
choiyoubin <48716638+choiyoubin@users.noreply.github.com>
2023-11-24 14:21:39 +0900

    fix - 홈 화면 수정

--- lib/modules/home/view/home_view.dart
+++ lib/modules/home/view/home_view.dart
@@ -40,43 +40,9 @@ class HomeScreen extends GetView<HomeController> {
                 SizedBox(
                   height: 27.h,
                 ),
-                //2. 검색 section
-                // GestureDetector(
-                //   onTap: () {
-                //     //TODO: move search View
-                //   },
-                //   child: Container(
-                //     padding:
-                //         EdgeInsets.symmetric(vertical: 12.h, horizontal: 14.w),
-                //     decoration: BoxDecoration(
-                //       color: AppColors.searchBackground,
-                //       borderRadius: BorderRadius.circular(8.r),
-                //     ),
-                //     child: Row(
-                //       mainAxisAlignment: MainAxisAlignment.start,
-                //       crossAxisAlignment: CrossAxisAlignment.center,
-                //       children: [
-                //         SvgPicture.asset(iconDir + "ic_search_16.svg"),
-                //         SizedBox(
-                //           width: 6.w,
-                //         ),
-                //         Text(
-                //           "고유 이름을 입력해보세요.",
-                //           style: AppTextStyles.body2Regular13
-                //               .copyWith(color: AppColors.grey4),
-                //           textHeightBehavior: TextHeightBehavior(
-                //               applyHeightToFirstAscent: false),
-                //         ),
-                //       ],
-                //     ),
-                //   ),
-                // ),
-                // SizedBox(
-                //   height: 20.h,
-                // ),
                 //3. 배너 section
                 SizedBox(
-                  height: 200.h,
+                  height: 250.h,
                   child: CarouselSlider.builder(
                     itemCount: controller.adCount.value,
                     options: CarouselOptions(
@@ -140,87 +106,6 @@ class HomeScreen extends GetView<HomeController> {
                     },
                   ),
                 ),
-              ],
-            ),
-          ),
-
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 29.h).copyWith(bottom: 0.h),
-            color: AppColors.searchBackground,
-            height: 10.h,
-          ),
-          //5. 매치 추천 section
-          // Padding(
-          //   padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 7.h),
-          //   child: Column(
-          //     children: [
-          //       CommonSectionHeader(
-          //         title: '오늘 이 매치는 어때요?',
-          //         //TODO : destination 임시 처리
-          //         destination: () async {
-          //           Get.to(TodayMatchScreen());
-          //         },
-          //       ),
-          //       SizedBox(
-          //         height: 223.h,
-          //         child: ListView.separated(
-          //           scrollDirection: Axis.horizontal,
-          //           itemCount: 2,
-          //           itemBuilder: (context, index) {
-          //             //TODO: 현재 같은 변수를 사용하여 좋아요가 동기화 됨
-          //             return TodayMatchItem(
-          //                 title: "후원 함께할분!",
-          //                 organization: "후원처명",
-          //                 count: 14,
-          //                 isLike: controller.isLike);
-          //           },
-          //           separatorBuilder: (context, index) {
-          //             return SizedBox(
-          //               width: 20.w,
-          //             );
-          //           },
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-
-          // Container(
-          //   color: AppColors.searchBackground,
-          //   height: 10.h,
-          // ),
-          //6. 기부처 추천 section
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 7.h),
-            child: Column(
-              children: [
-                CommonSectionHeader(
-                    title: '박레이님께 꼭 맞는 기부처 추천',
-                    //TODO : destination 임시 처리
-                    destination: () async {
-                      Get.toNamed(Routes.main_match);
-                    }),
-                SizedBox(
-                  height: 99.h,
-                  child: ListView.separated(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 2,
-                    itemBuilder: (context, index) {
-                      return OrganizationItem(
-                        title: "test",
-                        comment: "testest",
-                      );
-                    },
-                    separatorBuilder: (context, index) {
-                      return SizedBox(
-                        width: 20.w,
-                      );
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 25.h,
-                )
               ],
             ),
           ),
