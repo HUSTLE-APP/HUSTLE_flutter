3d374e1a16456fc38d25da8e11f2c260990ab8cb
onejuneee <onejuneee721@gmail.com>
2023-11-27 05:46:43 +0900

    Feat: 대회 화면 추가 퍼블리싱

--- lib/modules/match/view/match_view.dart
+++ lib/modules/match/view/match_view.dart
@@ -3,7 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import '../controller/match_controller.dart';
-
+import '../widget/custom_component.dart';
 
 class MatchScreen extends GetView<MatchController> {
   const MatchScreen({super.key});
@@ -30,28 +30,28 @@ class MatchScreen extends GetView<MatchController> {
             ),
           ],
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          showUnselectedLabels: true,
-          currentIndex: controller.selectIdx.value,
-          onTap: (value) => controller.selectIdx.value = value,
-          selectedItemColor: Colors.grey[900],
-          unselectedItemColor: Colors.grey[300],
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: '홈',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.event),
-              label: '이벤트',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'MY',
-            ),
-          ],
-        ),
+        // bottomNavigationBar: BottomNavigationBar(
+        //   type: BottomNavigationBarType.fixed,
+        //   showUnselectedLabels: true,
+        //   currentIndex: controller.selectIdx.value,
+        //   onTap: (value) => controller.selectIdx.value = value,
+        //   selectedItemColor: Colors.grey[900],
+        //   unselectedItemColor: Colors.grey[300],
+        //   items: [
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.home),
+        //       label: '홈',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.event),
+        //       label: '이벤트',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.person),
+        //       label: 'MY',
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
@@ -80,22 +80,22 @@ class BasketballCardWidget extends StatelessWidget {
         children: <Widget>[
           Image.asset(
             'asset/image/iv_test_contest.jpeg',
-            width: 300.0, // 가로 크기 조정
-            height: 200.0, // 세로 크기 조정
-            fit: BoxFit.cover, // 이미지가 할당된 공간을 어떻게 채울지 정의 (옵션)
+            width: 300.w,
+            height: 200.h,
+            fit: BoxFit.cover,
           ),
           Text(tournamentName),
-          Text(registrationPeriod),
-          Text(participantCount),
-          Text(location),
-          ElevatedButton(
-            onPressed: () {
-              // 신청하기 버튼 로직
-            },
-            child: const Text('신청하기'),
-          ),
+          // 여기에 새로운 Container 추가
+          CustomComponent(),
+          // ElevatedButton(
+          //   onPressed: () {
+          //     // 신청하기 버튼 로직
+          //   },
+          //   child: const Text('신청하기'),
+          // ),
         ],
       ),
     );
   }
 }
+
